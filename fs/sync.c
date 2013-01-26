b7496b9998d87f1e3895d846d318fdaf6d8ef45a
Boy Petersen <boypetersen@gmail.com>
2013-01-26 15:37:22 +0100

    added CONFIG_FSYNC_CONTROL
    FSYNC control appeared also in std version, which was not
    supposed to happen.

--- fs/sync.c
+++ fs/sync.c
@@ -18,8 +18,10 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
+#ifdef CONFIG_FSYNC_CONTROL
 bool fsync_enabled = true;
 module_param(fsync_enabled, bool, 0755);
+#endif
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -142,8 +144,10 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	int ret;
 	int fput_needed;
 
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 
 	file = fget_light(fd, &fput_needed);
 	if (!file)
@@ -174,8 +178,10 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 	struct address_space *mapping = file->f_mapping;
 	int err, ret;
 
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 
 	if (!file->f_op || !file->f_op->fsync) {
 		ret = -EINVAL;
@@ -209,8 +215,11 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
+
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 			
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
@@ -221,8 +230,10 @@ static int do_fsync(unsigned int fd, int datasync)
 	struct file *file;
 	int ret = -EBADF;
 
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 
 	file = fget(fd);
 	if (file) {
@@ -234,16 +245,20 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 			
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 			
 	return do_fsync(fd, 1);
 }
@@ -258,8 +273,10 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
  */
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 			
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
 		return 0;
@@ -325,8 +342,10 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	int fput_needed;
 	umode_t i_mode;
 
+#ifdef CONFIG_FSYNC_CONTROL
 	if (!fsync_enabled)
 			return 0;
+#endif
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
