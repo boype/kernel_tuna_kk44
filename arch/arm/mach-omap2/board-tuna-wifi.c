aad5e33a22fe070fca50c77dcef7533b24ae0cb1
Boy Petersen <boypetersen@gmail.com>
2014-08-17 17:18:07 +0200

    cleanup commit: tuna-wifi: do not randomize MAC if bootloader doesn't pass it

--- arch/arm/mach-omap2/board-tuna-wifi.c
+++ arch/arm/mach-omap2/board-tuna-wifi.c
@@ -299,7 +299,6 @@ static int tuna_wifi_get_mac_addr(unsigned char *buf)
 		return -EFAULT;
 
 	if ((tuna_mac_addr[4] == 0) && (tuna_mac_addr[5] == 0)) {
-		srandom32((uint)jiffies);
 		omap_get_die_id(&oid);
 		rand_mac = (uint)oid.id_3; // id_3 or id_1 ?
 		tuna_mac_addr[3] = (unsigned char)rand_mac;
